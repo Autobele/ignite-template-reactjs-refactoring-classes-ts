7d7a10dc55395c849a15e7cc2c34965da0db59ea
Autobele <autobele@infoway-pi.com.br>
2021-09-06 20:13:48 -0300

    Convert all archives to typescript

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -9,8 +9,15 @@ import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface IInputProps {
+  name: string,
+  icon?: any
+  rest?: [],
+  placeholder: string,
+}
+
+const Input: React.FC<IInputProps> = ({ name, icon: Icon, ...rest }) => {
+  const inputRef = useRef<any>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,6 +1,12 @@
 import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+
+interface IContainerStyled {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<IContainerStyled>`
   display: flex;
   align-items: center;
 
